bd9ed02feb208335c378d4e488ec71cb1f741c94
Rishabh bharota <rishabhbharota@gmail.com>
2022-08-25 18:47:52 +0530

    updated port for heroku

--- app.js
+++ app.js
@@ -55,8 +55,9 @@ app.post("/", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log("Server is running on port " + PORT);
 })
 
 // request browser to reload page
